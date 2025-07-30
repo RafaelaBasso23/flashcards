78abe680b9b0ab88848d47c3e6ea1cd8fa284c22
RafaelaBasso23 <rafaela.alves.freitas@escola.pr.gov.br>
2025-07-30 12:54:24 +0000

    criamos as perguntas do java

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    "Formula 1",
+    "Qual é a equipe mais vencedora da Formula 1?",
+    "Ferrari"
+)
+criaCartao(
+    "Formula 1",
+    "Quem tem mais titulos mundiais?",
+    "Lewis Hamilton (empatado com Schumacher"
+)
+criaCartao(
+    "Forumla 1",
+    "Qula piloto venceu mais corridas?",
+    "Lewis Hamilton"
+)
+criaCartao(
+    "Formula 1",
+    "Qual é o GP mai clasico da Formula 1?",
+    "Mônaco"
+)
+criaCartao(
+    "Formula 1",
+    "Quem é o piloto mais novo a ganhar corridas?",
+    "Max Verstappen"
+)
+criaCartao(
+    "Formula 1",
+    "Quantas rodas tem um carro de Formula 1?",
+    "Quatro rodas"
+)
+criaCartao(
+    "Formula 1",
+    "Qual é a velocidade maxima da formula 1 moderna?",
+    "Cera de 370km/h"
+)
+criaCartao(
+    "Formula 1",
+    "Quantos pilotos largam em uma corrida?",
+    "Vinte pilotos"
+)
+criaCartao(
+    "Formula 1",
+    "Qual país sediu mais GPs?",
+    "Italía"
+)
